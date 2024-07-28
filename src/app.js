e44a815f5af77a9569049240fcd63262194d95f9
Thiago Bussola <thiago.bussola@in8.com.br>
2024-07-27 23:31:48 -0300

    refactor: ajustando rotas e retornos

--- src/app.js
+++ src/app.js
@@ -1,41 +1,25 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import { createClient } from 'redis';
-import dotenv from 'dotenv';
-import ringsOfPowerController from './ringsOfPower/ringOfPower.controller.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import ringsOfPowerController from "./ringsOfPower/ringOfPower.controller.js";
 
 dotenv.config();
 
 const mongoConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error('Error connecting to MongoDB', error.message);
-  }
-};
-
-const redisConnection = async () => {
-  const redisClient = createClient({
-    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-  });
-
-  try {
-    await redisClient.connect();
-    console.log('Connected to Redis');
-    return redisClient;
-  } catch (error) {
-    console.error('Error connecting to Redis', error.message);
+    console.error("Error connecting to MongoDB", error.message);
   }
 };
 
 const app = express();
 
 mongoConnection();
-const redisClient = await redisConnection();
 
 app.use(express.json());
-app.use(ringsOfPowerController(redisClient));
+app.use(ringsOfPowerController());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
